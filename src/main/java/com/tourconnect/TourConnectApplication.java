95b72a2b9a4329e51939fd7c4acf076e0af4ba41
Durga Prasad <durgaprasadkar1998.dp@gmail.com>
2026-05-31 01:02:06 +0530

    Initial Repo for the TourConnect Backend application

---
+++ src/main/java/com/tourconnect/TourConnectApplication.java
@@ -0,0 +1,13 @@
+package com.tourconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tourconnect/TourConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
